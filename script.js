b4447c84d762c497495cdd35c2ad9b9cc87126e9
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-02 15:20:51 +0300

    updated bmı calculator

--- script.js
+++ script.js
@@ -12,43 +12,37 @@ window.addEventListener("scroll", function () {
   }
 });
 
-//BMI Calculator
+// BMI Calculator
 
 function calculateBMI() {
-  //Selecting Elements
-  let heightInput = parseInt(document.querySelector("#height").value);
-  let weightInput = parseInt(document.querySelector("#weight").value);
-  let result = document.querySelector("#result").value;
-
-  // Checking if inputs are valid numbers
-  if (isNaN(heightInput) || isNaN(weightInput)) {
-    result.textContent = "Please enter valid numbers.";
-    return;
+    // Selecting Elements
+    let heightInput = parseInt(document.querySelector('#height').value);
+    let weightInput = parseInt(document.querySelector('#weight').value);
+    let result = document.querySelector('#result');
+  
+    // Checking if inputs are valid numbers
+    if (isNaN(heightInput) || isNaN(weightInput)) {
+      result.textContent = "Please enter valid numbers.";
+      return;
+    }
+  
+    // Calculating BMI
+    let bmi = (weightInput / ((heightInput * heightInput) / 10000)).toFixed(1);
+  
+    // Displaying Result
+    if (bmi < 18.5) {
+      result.textContent =  bmi +"-> Underweight" ;
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      result.textContent = bmi +"-> Normal";
+    } else if (bmi >= 25 && bmi < 29.9) {
+      result.textContent = bmi +"-> Overweight";
+    } else if (bmi >= 30 && bmi < 39.9) {
+      result.textContent =  bmi +"-> Obese";
+    } else {
+      result.textContent = bmi +"->Extremely Obese";
+    }
   }
-
-  // Calculating BMI
-  let bmi = (weightInput / ((heightInput * heightInput) / 10000)).toFixed(1);
-
-  if (bmi < 18.5) {
-
-    document.querySelector("#result").textContent = "Zayıf : " + `${result}`;
-  } else if (bmi >= 18.5 && bmi < 24.9) {
-
-    document.querySelector("#result").textContent = "Normal : " + `${result}`;
-  } else if (bmi >= 25 && bmi < 29.9) {
-
-    document.querySelector("#result").textContent =
-      "Fazla Kilolu : " + `${result}`;
-
-  } else if (bmi >= 30 && bmi < 39.9) {
-    document.querySelector("#result").textContent = "Obez : " + `${result}`;
-  } else {
-
-    document.querySelector("#result").textContent =
-      "Aşırı Obez :" + `${result}`;
-  }
-}
-
-// Adding event listeners to input fields
-document.querySelector('#height').addEventListener('keyup', calculateBMI);
-document.querySelector('#weight').addEventListener('keyup', calculateBMI);
+  
+  // Adding event listeners to input fields
+  document.querySelector('#height').addEventListener('keyup', calculateBMI);
+  document.querySelector('#weight').addEventListener('keyup', calculateBMI);
