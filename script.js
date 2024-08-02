263f650e2cbf5227331abdd460ec876a57693ea4
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-02 18:30:26 +0300

    updated script.js

--- script.js
+++ script.js
@@ -15,34 +15,36 @@ window.addEventListener("scroll", function () {
 // BMI Calculator
 
 function calculateBMI() {
-    // Selecting Elements
-    let heightInput = parseInt(document.querySelector('#height').value);
-    let weightInput = parseInt(document.querySelector('#weight').value);
-    let result = document.querySelector('#result');
-  
-    // Checking if inputs are valid numbers
-    if (isNaN(heightInput) || isNaN(weightInput)) {
-      result.textContent = "Please enter valid numbers.";
-      return;
-    }
-  
-    // Calculating BMI
-    let bmi = (weightInput / ((heightInput * heightInput) / 10000)).toFixed(1);
-  
-    // Displaying Result
-    if (bmi < 18.5) {
-      result.textContent =  bmi +"-> Underweight" ;
-    } else if (bmi >= 18.5 && bmi < 24.9) {
-      result.textContent = bmi +"-> Normal";
-    } else if (bmi >= 25 && bmi < 29.9) {
-      result.textContent = bmi +"-> Overweight";
-    } else if (bmi >= 30 && bmi < 39.9) {
-      result.textContent =  bmi +"-> Obese";
-    } else {
-      result.textContent = bmi +"->Extremely Obese";
-    }
+  // Selecting Elements
+  let heightInput = parseInt(document.querySelector("#height").value);
+  let weightInput = parseInt(document.querySelector("#weight").value);
+  let result = document.querySelector("#result");
+  const triangle = document.querySelector(".triangle");
+
+  // Checking if inputs are valid numbers
+  if (isNaN(heightInput) || isNaN(weightInput)) {
+    result.textContent = "Please enter valid numbers.";
+    return;
   }
-  
-  // Adding event listeners to input fields
-  document.querySelector('#height').addEventListener('keyup', calculateBMI);
-  document.querySelector('#weight').addEventListener('keyup', calculateBMI);
+
+  // Calculating BMI
+  let bmi = (weightInput / ((heightInput * heightInput) / 10000)).toFixed(1);
+
+  // Displaying Result
+  if (bmi < 18.5) {
+    result.textContent = bmi + "-> Underweight";
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    result.textContent = bmi + "-> Normal";
+  } else if (bmi >= 25 && bmi < 29.9) {
+    result.textContent = bmi + "-> Overweight";
+  } else if (bmi >= 30 && bmi < 39.9) {
+    result.textContent = bmi + "-> Obese";
+  } else {
+    result.textContent = bmi + "->Extremely Obese";
+  }
+}
+
+// Adding event listeners to input fields
+document.querySelector("#height").addEventListener("keyup", calculateBMI);
+document.querySelector("#weight").addEventListener("keyup", calculateBMI);
+
