4534d4098c9099034c5ff840e9a67f1611ae9898
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-09 00:33:39 +0300

    Refined calculations to enhance precision across the 'Underweight', 'Normal', 'Overweight', 'Obese', and 'Extremely Obese' ranges.

--- script.js
+++ script.js
@@ -55,28 +55,28 @@ function calculateBMI() {
 
   // Displaying Result
   if (bmi < 18.5) {
-    result.textContent = bmi + "-> Underweight";
-    const range1 = 6 + ((bmi- 13.5) * 3.2);
+    result.textContent = bmi + " -> Underweight";
+    const range1 = (bmi - 10) * (100 / 45);  
     triangle.style.left = `${range1}%`;
 
-  } else if (bmi >= 18.5 && bmi < 24.9) {
-    result.textContent = bmi + "-> Normal";
-    const range2 = 22 + ((bmi - 18.5) * 2.3);
+  } else if (bmi >= 18.5 && bmi < 25) {
+    result.textContent = bmi + " -> Normal";
+    const range2 = ((bmi - 18.5) * (100 / 45)) + 20; 
     triangle.style.left = `${range2}%`;
 
-  } else if (bmi >= 25 && bmi < 29.9) {
-    result.textContent = bmi + "-> Overweight";
-    const range3 = 38 + ((bmi - 25) * 2.4);;
+  } else if (bmi >= 25 && bmi < 30) {
+    result.textContent = bmi + " -> Overweight";
+    const range3 = ((bmi - 25) * (100 / 45)) + 40;  
     triangle.style.left = `${range3}%`;
 
-  } else if (bmi >= 30 && bmi < 39.9) {
-    result.textContent = bmi + "-> Obese";
-    const range4 = 54 + (bmi - 30) * 2.9;
+  } else if (bmi >= 30 && bmi < 35) {
+    result.textContent = bmi + " -> Obese";
+    const range4 = ((bmi - 30) * (100 / 45)) + 54;  
     triangle.style.left = `${range4}%`;
 
   } else {
-    result.textContent = bmi + "->Extremely Obese";
-    const range5 = 70 + (bmi - 35) * 3.1;
+    result.textContent = bmi + " -> Extremely Obese";
+    const range5 = ((bmi - 35) * (100 / 45)) + 70;  
     triangle.style.left = `${range5}%`;
   }
 }
