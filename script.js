e1907f37f238272550180bce828c269714d5c4a6
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-08 18:35:29 +0300

    edited hamburger-menu

--- script.js
+++ script.js
@@ -83,5 +83,3 @@ function toggleMenu() {
   const navLinks = document.querySelector('.nav-links');
   navLinks.classList.toggle('show');
 }
-
-document.querySelector('#hamburger').addEventListener('click', toggleMenu);
